3a06b4fedbfbd6afdb86b6f3832cbca985e94539
Namlook <n.namlook@gmail.com>
2010-11-11 21:30:58 +0100

    clean up

--- trackeet/views/entry.py
+++ trackeet/views/entry.py
@@ -12,7 +12,6 @@ entry = Module(__name__)
 
 
 @entry.route('/entry/new')
-@entry.route('/')
 def new_entry():
     """
     display the form page in order to create a new entry
@@ -51,14 +50,14 @@ def create_entry():
     entry['created_at'] = datetime.utcnow()
     entry.save()
     flash('entry created', 'success') 
-    return redirect(url_for('new_entry'))
+    return redirect(url_for('list_entries'))
     
 @entry.route('/entry/edit/<id>')
 def edit_entry(id):
     entry = g.db.Entry.find_one(ObjectId(id))
     if not entry:
         flash('entry not found', 'error')
-        return redirect(url_for('new_entry'))
+        return redirect(url_for('list_entries'))
     return render_template('edit_entry.html', entry=entry)
 
 @entry.route('/entry/update/<id>', methods=['POST'])
@@ -90,16 +89,32 @@ def update_entry(id):
             entry['project'] = project_id
         entry.save()
         flash('entry updated', 'success')
-    return redirect(url_for('new_entry'))
+    return redirect(url_for('list_entries'))
 
-@entry.route('/entry/list/<project>')
+@entry.route('/')
 @entry.route('/entry/list')
-def list_entries(project=None):
+def list_entries():
     query = {}
-    if project:
-        query['project'] = project
+    if request.args.get('project'):
+        query['project'] = request.args['project']
+    if request.args.get('tag'):
+        query['tags'] = request.args['tag']
     entries = g.db.Entry.find(query)
-    return render_template('list_entries.html', entries=entries)
+    total_time = 0
+    for entry in g.db.Entry.find(query, fields=['duration']):
+        total_time += entry['duration']
+    return render_template('list_entries.html', entries=entries,
+      total_time=total_time)
+
+@entry.route('/entry/delete/<id>', methods=['post'])
+def delete_entry(id):
+    entry = g.db.Entry.find_one(ObjectId(id))
+    if entry:
+        entry.delete()
+        flash('entry deleted', 'success')
+    else:
+        abort(404)
+    return redirect(url_for('list_entries'))
 
 @entry.route('/ajax/project/list', methods=['GET'])
 def ajax_list_projects():
@@ -110,8 +125,4 @@ def ajax_list_projects():
       mimetype='application/json'
     )
 
-@entry.route('/tag/<id>')
-def list_entry_by_tag(id):
-    entries = g.db.Entry.find({'tags':id})
-    return render_template('list_entry_by_tag.html',
-      entries=entries, current_tag=id)
+
